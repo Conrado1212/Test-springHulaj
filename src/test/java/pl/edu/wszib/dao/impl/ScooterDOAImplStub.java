721d68a47ffde5eb4e3e2dd1ef3ffdc8e9e96cff
Conrado1212 <conrado1212@gmail.com>
2020-01-16 15:17:32 +0100

    Test-springHulaj

--- src/test/java/pl/edu/wszib/dao/impl/ScooterDOAImplStub.java
+++ src/test/java/pl/edu/wszib/dao/impl/ScooterDOAImplStub.java
@@ -3,6 +3,7 @@ package pl.edu.wszib.dao.impl;
 import pl.edu.wszib.dao.IScooterDAO;
 import pl.edu.wszib.model.Scooter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ScooterDOAImplStub implements IScooterDAO {
@@ -31,19 +32,87 @@ public class ScooterDOAImplStub implements IScooterDAO {
     public void removeScooterId(int Id) {
 
     }
-
+    List<Scooter> scooters = new ArrayList<>();
     @Override
-    public List<Scooter> getAllScooterXiaomi() {
-        return null;
+    public List<Scooter>  getAllScooterXiaomi(){
+
+    Scooter scooter = new Scooter();
+
+        scooter.setId(1);
+        scooter.setBrand("Xiaomi");
+        scooter.setModel("Mijia M365");
+        scooter.setPrice(40);
+
+        Scooter scooter1 = new Scooter();
+        scooter1.setId(2);
+        scooter1.setBrand("Xiaomi");
+        scooter1.setModel("Mijia M365 PRO");
+        scooter1.setPrice(40);
+
+
+
+        this.scooters.add(scooter);
+        this.scooters.add(scooter1);
+
+
+        return scooters;
+
     }
 
     @Override
     public List<Scooter> getAllScooters() {
-        return null;
+        Scooter scooter = new Scooter();
+
+        scooter.setId(1);
+        scooter.setBrand("Xiaomi");
+        scooter.setModel("Mijia M365");
+        scooter.setPrice(40);
+
+        Scooter scooter1 = new Scooter();
+        scooter1.setId(2);
+        scooter1.setBrand("Xiaomi");
+        scooter1.setModel("Mijia M365 PRO");
+        scooter1.setPrice(45);
+
+        Scooter scooter2 = new Scooter();
+        scooter2.setId(3);
+        scooter2.setBrand("Fiat");
+        scooter2.setModel("M500");
+        scooter2.setPrice(30);
+
+        Scooter scooter3 = new Scooter();
+
+        scooter3.setId(4);
+        scooter3.setBrand("Fiat");
+        scooter3.setModel("M500 8.5");
+        scooter3.setPrice(40);
+
+        this.scooters.add(scooter);
+        this.scooters.add(scooter1);
+        this.scooters.add(scooter2);
+        this.scooters.add(scooter3);
+
+        return scooters;
+
     }
 
     @Override
     public List<Scooter> getAllScooterFiat() {
-        return null;
+        Scooter scooter2 = new Scooter();
+        scooter2.setId(3);
+        scooter2.setBrand("Fiat");
+        scooter2.setModel("M500");
+        scooter2.setPrice(30);
+
+        Scooter scooter3 = new Scooter();
+
+        scooter3.setId(4);
+        scooter3.setBrand("Fiat");
+        scooter3.setModel("M500 8.5");
+        scooter3.setPrice(40);
+
+        this.scooters.add(scooter2);
+        this.scooters.add(scooter3);
+        return scooters;
     }
 }
